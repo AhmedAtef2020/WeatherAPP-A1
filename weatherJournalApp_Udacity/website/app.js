16c7d0ae726536b1d54d782f730fd2d95f4f758d
AhmedAtef <ahmedatef1428@gmail.com>
2022-12-28 00:28:06 +0200

    Get the data object from API

--- weatherJournalApp_Udacity/website/app.js
+++ weatherJournalApp_Udacity/website/app.js
@@ -1,5 +1,80 @@
 /* Global Variables */
+const baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip=';
+const apiKey = '&appid=1a49afd7c84c376f8170c1f2bd2576de&units=imperial';
+let generateBtn = document.getElementById('generate');
+
+// console.log(zipCode);
 
 // Create a new date instance dynamically with JS
+
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+
+
+const postApiData = async ( url='', apiData = {}) => {
+    console.log(apiData);
+      const resPost = await fetch(url, {
+      method: 'POST',
+      credentials: 'same-origin',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(apiData),       
+    });
+  
+      try {
+        const allResPost = await resPost.json();
+        console.log(allResPost);
+        return allResPost;
+      }catch(error) {
+      console.log("error", error);
+      }
+  }
+
+
+
+
+
+async function getApiData() {
+    let zipCode = document.getElementById('zip').value;
+    let feelingRes = document.getElementById('feelings').value;
+    console.log(zipCode);
+
+    try {
+        const apiResponse = await fetch(baseURL+zipCode+apiKey);
+        const apiData = await apiResponse.json();
+        const WeatherTemp = apiData.main.temp;
+        console.log(WeatherTemp); // for the temperature
+
+        // console.log(apiData); // all the data object
+        // console.log(apiData.name); // for City name
+        // console.log(apiData.sys.country); // for Country name
+        // console.log(apiData.main.temp); // for the temperature
+        // console.log(apiData.weather[0].main); // for weather skys
+        postApiData('/apiData', {newDate, feelingRes, WeatherTemp});
+        return WeatherTemp;
+    } catch (error) {
+        console.log('The Error : ', error);
+    }
+}
+
+generateBtn.addEventListener('click', getApiData);
+
+// getData();  // to confirm the function running
+
+/** tried to use this way at first */
+
+// generateBtn.addEventListener('click', async () => {
+//     let zipCode = document.getElementById('zip').value;
+// try {
+//     const apiResponse = await fetch(baseURL+zipCode+apiKey);
+//     const apiData = await apiResponse.json();
+//     console.log(apiData);
+//     return apiData;
+// } catch (err) {
+//     console.log('The Error : ', err);
+// }
+// });
+
+
